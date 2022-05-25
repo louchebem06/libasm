c54f1d90b6039cff9fbe092c07eb0876738cfcc8
Ledda Bryan <louchebem06@gmail.com>
2022-05-25 17:28:25 +0200

    UPDATE: atoi_base is not fonctinal

--- src/ft_atoi_base_bonus.s
+++ src/ft_atoi_base_bonus.s
@@ -9,14 +9,80 @@ _ft_atoi_base:
 	mov		rbp, rsp ; classic init
 	mov		rbx, rdi ; save str
 	mov		rdi, rsi ; base in first arg
+	call	_ft_strlen ; call strlen(base)
+	mov		r15, rax ; r15 = strlen(base)
+	mov		rdi, rsi ; base in first arg
 	call	check_size ; call check_size(base)
 	mov		rdi, rsi ; base in first arg
 	call	check_duplicate ; call check_duplicate(base)
 	mov		rdi, rsi ; base in first arg
 	call	check_forbidden_char ; call check_forbidden_char(base)
-	mov		rdi, rbx
-	call	_ft_strlen
+	mov		rdi, rbx ; base in first arg
+	call	skip_white_space
+	xor 	rax, rax ; ret = 0
+	xor		r8, r8 ; r8 plus = 0
+	xor 	r9, r9 ; r9 minus = 0
+while_sign:
+	cmp		byte [rdi], 0 ; if *str == nul
+	je		leave_return
+	call	r8_r9_sign
+	inc		rdi ; rdi++
+	cmp		byte [rdi], '+'
+	je		while_sign ; continue
+	cmp		byte [rdi], '-'
+	je		while_sign ; continue
+while_atoi:
+	cmp		byte [rdi], 0 ; if *str == nul
+	je		the_end
+	imul	rax, r15 ; ret *= strlen(base)
+	mov		r14, rax ; save ret
+	call	index_base ; call index_base
+	jmp		leave_return
+	cmp		rax, 0 ; if index_base < 0
+	jl		the_end
+	add		r14, rax ; save += index_base
+	mov 	rax, r14 ; rat = save
+	inc		rdi; str++
+	jmp		while_atoi
+the_end:
+	; cmp		r8, r9 ; if + < -
+	; je		isneg; rax *= -1
 	leave
+	ret
+isneg:
+	; mul		rax, -1
+	ret
+
+index_base:
+	xor		r10, 0 ; r10 = 0
+	mov		r11, rsi ; r11 = rsi
+	mov		r12b, byte [rdi]
+while_index_base:
+	cmp		byte [r11], r12b
+	je 		found
+	cmp		byte [r11], 0
+	je 		not_found
+	inc 	r10 ; r10++
+	inc		r11 ; r11++
+	jmp		while_index_base
+found:
+	mov		rax, r10;
+	ret 
+not_found:
+	mov		rax, -1
+	ret
+
+r8_r9_sign:
+	cmp		byte [rdi], '+'
+	je		r8_p
+	cmp		byte [rdi], '-'
+	je		r9_p
+	ret
+r8_p:
+	inc r8
+	ret
+r9_p:
+	inc r9
 	ret
 
 check_size:
@@ -54,6 +120,17 @@ isspace:
 	je		true
 	jmp		false
 
+skip_white_space:
+	cmp		byte [rdi], 0 ;
+	je		not_good
+	call	isspace ; isspace(rdi)
+	cmp		rax, 1 ; if true
+	je		inc_rdi ; continue
+	ret ; return
+inc_rdi:
+	inc 	rdi
+	jmp 	skip_white_space
+
 check_forbidden_char:
 	cmp		byte [rdi], 0 ; if *rsi == 0
 	je		return
@@ -71,6 +148,9 @@ not_good:
 	mov		rax, 0 ; ret = 0
 	leave ; clear quit
 	ret ; return
+leave_return:
+	leave
+	ret
 return:
 	ret
 true:
