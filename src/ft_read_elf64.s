f7b76099d5d083dbd6c2e5a6b2ad8c76709f3448
Ledda Bryan <louchebem06@gmail.com>
2022-05-25 14:45:54 +0200

    FIX: read linux

--- src/ft_read_elf64.s
+++ src/ft_read_elf64.s
@@ -8,14 +8,14 @@ _ft_read:
 	mov		rax, SYS_READ ; ret = read()
 	syscall ; call system
 	cmp rax, 0 ; if return < 0
-  	je errno ; goto errno
+  	jl errno ; goto errno
 	leave ; clear
 	ret ; return
 errno:
 	neg rax
-	mov rdi, rax ; value WRITE in first arg
+	mov rdi, rax ; value READ in first arg
     call __errno_location ; call error()
-	mov [rax], rdi ; errno = return write
+	mov [rax], rdi ; errno = return read
 	mov rax, -1 ; ret = -1
 	leave ; clear
 	ret ; return
--- test/ft_read_test.cpp
+++ test/ft_read_test.cpp
@@ -6,22 +6,17 @@ void ft_read_test(void) {
 	ptrRead rd[2] = { &FT_READ, &read };
 	int ret;
 
-	cout << "== read() ==" << endl;
-	for (int i = 0; i < 2; i++) {
-		cout << "Please input for test READ " << (!i ? "FT" : "STD") << ": " << flush;
-		ret = rd[i](1, buff[i], 1024);
-		(ret == -1) ? perror((!i) ? "FT" : "STD") : (void)(cout << buff[i] << endl << "RET : " << ret << endl);
-	}
 	cout << "== Test file ==" << endl;
 	for (int i = 0; i < 2; i++) {
-		ret = rd[i](1, buff[i], 1024);
-		int fd = open("text.txt", O_RDONLY);
-		(ret == -1) ? perror((!i) ? "FT" : "STD") : (void)(cout << buff[i] << endl << "RET : " << ret << endl);
+		int fd = open("test/text.txt", O_RDONLY);
+		ret = rd[i](fd, buff[i], 1024);
+		(ret == -1) ? perror((!i) ? "FT" : "STD") : (void)(cout << buff[i] << "RET : " << ret << endl);
 		close(fd);
+		cout << endl;
 	}
 	cout << "== Test invalid fd ==" << endl;
 	for (int i = 0; i < 2; i++) {
-		ret = rd[i](1, buff[i], 1024);
+		ret = rd[i](-42, buff[i], 1024);
 		(ret == -1) ? perror((!i) ? "FT" : "STD") : (void)(cout << buff[i] << endl << "RET : " << ret << endl);
 	}
 	cout << endl;
