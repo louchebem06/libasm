5e6db51bf9656f0ffc991d8059f05709c28c047d
Ledda Bryan <louchebem06@gmail.com>
2022-05-25 02:39:51 +0200

    UPDATE

--- test/ft_atoi_base_test.cpp
+++ test/ft_atoi_base_test.cpp
@@ -5,12 +5,13 @@ void ft_atoi_base_test(void) {
 		make_pair("125", "0123456789"),
 		make_pair("1111101", "01"),
 		make_pair("7D", "0123456789ABCDEF"),
-		make_pair("uoi", "choumi")
+		make_pair("uoi", "choumi"),
+		make_pair("error", "-asd")
 	};
 
 	for (pair<string, string> p : tab) {
 		int ret = ft_atoi_base(p.first, p.second);
 		cout << ret << endl;
-		cout << ((ret == ((p.first == "uoi") ? 0 : 125)) ? "✅" : "❌") << endl;
+		cout << ((ret == ((p.first == "error") ? 0 : 125)) ? "✅" : "❌") << endl;
 	}
 }
